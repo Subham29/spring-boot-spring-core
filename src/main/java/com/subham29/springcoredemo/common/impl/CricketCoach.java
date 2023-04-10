acf27dd5754a05f2530d0a826d4d1591d44e23e1
Subham <subham.patel@temenos.com>
2023-04-10 23:08:17 +0530

    Bean Scopes by introducing Check API

--- src/main/java/com/subham29/springcoredemo/common/impl/CricketCoach.java
+++ src/main/java/com/subham29/springcoredemo/common/impl/CricketCoach.java
@@ -1,9 +1,12 @@
 package com.subham29.springcoredemo.common.impl;
 
 import com.subham29.springcoredemo.common.api.Coach;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
     CricketCoach() {
         System.out.println(getClass().getSimpleName());
--- src/main/java/com/subham29/springcoredemo/controller/DemoController.java
+++ src/main/java/com/subham29/springcoredemo/controller/DemoController.java
@@ -8,16 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    private final Coach myCoach;
+    private Coach myCoach;
+    private Coach anotherCoach;
 
     @Autowired
-    DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    DemoController(@Qualifier("cricketCoach") Coach theCoach, @Qualifier("cricketCoach") Coach theAnotherCoach) {
         System.out.println(getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkOut() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "<h1>Is Instance Same: " + (myCoach == anotherCoach) + "</h1>";
     }
 }
